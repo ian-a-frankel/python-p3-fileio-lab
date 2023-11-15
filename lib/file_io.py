7bdaf971e37d0851437275d27d8b7f30b11a4c00
Ian Frankel <ian.frankel@gmail.com>
2023-11-15 13:29:20 -0500

    readin and writin

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode = 'w', encoding='utf-8') as overwriting_file:
+        overwriting_file.write(file_content)
+
+# with open('log_file.txt', mode='w', encoding='utf-8') as log_file:
+#     log_file.write('Log 1')
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode = 'a', encoding='utf-8') as appending_file:
+        appending_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8') as file_to_read:
+        return(file_to_read.read())
